2a724c92999cdd539c0089677c1e3eb6c0140160
Orji Chidi Matthew <orjichidi95@gmail.com>
2022-01-06 03:14:18 +0100

    feat: transactions working

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from "react";
 import PrimaryButton from "../components/primary-button";
+import abi from "../utils/Keyboards.json";
+import { ethers } from "ethers";
 
 export default function Home() {
   const [ethereum, setEthereum] = useState(undefined);
   const [connectedAccount, setConnectedAccount] = useState(undefined);
+  const [keyboards, setKeyboards] = useState([]);
+  const [newKeyboard, setNewKeyboard] = useState("");
+
+  const contractAddress = "0x9F7D6ADDA4C07e049C31B990b9110571F344d46c";
+  const contractABI = abi.abi;
 
   const handleAccounts = (accounts) => {
     if (accounts.length > 0) {
@@ -37,6 +44,48 @@ export default function Home() {
     handleAccounts(accounts);
   };
 
+  const getKeyboards = async () => {
+    if (ethereum && connectedAccount) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const keyboardsContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      const keyboards = await keyboardsContract.getKeyboards();
+      console.log("Retrieved keyboards...", keyboards);
+      setKeyboards(keyboards);
+    }
+  };
+  useEffect(() => getKeyboards(), [connectedAccount]);
+
+  const submitCreate = async (e) => {
+    e.preventDefault();
+
+    if (!ethereum) {
+      console.error("Ethereum object is required to create a keyboard");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const keyboardsContract = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      signer
+    );
+
+    const createTxn = await keyboardsContract.create(newKeyboard);
+    console.log("Create transaction started...", createTxn.hash);
+
+    await createTxn.wait();
+    console.log("Created keyboard!", createTxn.hash);
+
+    await getKeyboards();
+  };
+
   if (!ethereum) {
     return <p>Please install MetaMask to connect to this site</p>;
   }
@@ -49,5 +98,37 @@ export default function Home() {
     );
   }
 
-  return <p>Connected Account: {connectedAccount}</p>;
+  return (
+    <div className="flex flex-col gap-y-8">
+      <form className="flex flex-col gap-y-2">
+        <div>
+          <label
+            htmlFor="keyboard-description"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Keyboard Description
+          </label>
+        </div>
+        <input
+          name="keyboard-type"
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          value={newKeyboard}
+          onChange={(e) => {
+            setNewKeyboard(e.target.value);
+          }}
+        />
+        <PrimaryButton type="submit" onClick={submitCreate}>
+          Create Keyboard!
+        </PrimaryButton>
+      </form>
+
+      {
+        <div>
+          {keyboards.map((keyboard, i) => (
+            <p key={i}>{keyboard}</p>
+          ))}
+        </div>
+      }
+    </div>
+  );
 }
